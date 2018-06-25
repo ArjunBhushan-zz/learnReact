4e32940d89a1f98354fac6767e3879a3854b47e7
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-06-25 18:47:32 -0400

    Initial commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hi, I am a React App</h1>
+    );
+  }
+}
+
+export default App;
